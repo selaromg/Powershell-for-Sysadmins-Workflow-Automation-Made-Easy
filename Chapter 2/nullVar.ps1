163405b778479f0bf736f89c13ed3e9164ef2dae
Gigi <selaromg13@gmail.com>
2025-02-24 15:04:52 -0500

    automatic and preference variables

--- Chapter 2/nullVar.ps1
+++ Chapter 2/nullVar.ps1
@@ -1,3 +1,12 @@
+Set-StrictMode -Version Latest
+
 $foo = $null
-$foo
-$bar
+$foo #no errors output since assigned null
+$bar #after setting strictmode verbose error shows denoting that variable has not been initialized
+
+PING.EXE -n 1 dfdfdfdfd.com
+#while domain does not exist -- exit code is hidden with pwsh automatic variable $LastExitCode 0 = success, 1 = failure
+$LASTEXITCODE
+
+PING.EXE -n 1 google.com
+$LASTEXITCODE
---
+++ Chapter 2/preferenceVars.ps1
@@ -0,0 +1,4 @@
+#preference vars control the default behavior of various output streams like Error, Warning, Verbose, Debug, and Information
+#get-variable with preference name filters displays all preference variables
+
+Get-Variable -Name *Preference
