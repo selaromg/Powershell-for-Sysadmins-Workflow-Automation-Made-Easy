56f559285982d9f8b47fed1070e88020e2cd4b55
Gigi <selaromg13@gmail.com>
2025-03-12 15:33:21 -0400

    more work on objects

--- Chapter 2/objects.ps1
+++ Chapter 2/objects.ps1
@@ -3,3 +3,6 @@
 # CLASSES specify what an object will contain
 
 $color = 'red' #creates a simple string object
+
+Select-Object -InputObject $color -Property * #displays all properties of called object
+# in this example, color only has 1 property called length
